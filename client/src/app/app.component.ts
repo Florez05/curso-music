b3e26f43e33389f3330225593642b6c15339fd6d
Sergio Flores <sergioflores@Sergio-Flores-MacBook-Air.local>
2021-10-29 15:00:26 -0500

    Angular con errores

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,30 +3,84 @@ import { UserService } from './services/user.service';
 import { User } from './models/user';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  providers: [UserService]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    providers: [UserService]
 })
 
 export class AppComponent implements OnInit {
-  public title = 'SPOTIFYY';
-  public user: User;
-  public identity = false; 
-  public token: any;
+    public title = 'SPOTIFYY';
+    public user: User;
+    public identity: any;
+    public token: any;
+    public errorMessageDiv = false;
+    public errorMessage = "";
 
-  constructor(
-    private _userService:UserService
-  ){
-    this.user = new User('','','','','','ROLE_USER','');
-  }
+    constructor(
+        private _userService: UserService
+    ) {
+        this.user = new User('', '', '', '', '', 'ROLE_ADMIN', '');
+    }
 
-  ngOnInit(){
-    /* var texto = this._userService.signup();
-    console.log(texto); */    
-  }
+    ngOnInit() {
+        /* var texto = this._userService.signup();
+        console.log(texto); */
+    }
 
-  public onSubmit(){
-    console.log(this.user);
-    
-  }
+    public onSubmit() {
+        console.log(this.user);
+        //conseguir los datos del usuario identificado
+        this._userService.signup(this.user).subscribe(
+            response => {
+                let identity = response;
+                this.identity = identity;
+
+                if (this.identity._id) {
+                    alert("el usuario no esta correctamente logueado");
+                }else{
+                    //crear elemento en el localStorage para tener al usuario sesion
+
+                    //conseguir el token para enviarselo a cada peticion hhtp
+                    this._userService.signup(this.user, true).subscribe( (response) => {
+                            let token = response.token;
+                            this.token = token;
+                            console.log("rta"+JSON.stringify(response));
+                            
+                            if (this.token.length <= 0) {
+                                alert("el token no se ha generado");
+                            }else{
+                                //crear elemento en el localStorage para tener el token disponible
+                                console.log(token);
+                                console.log(identity);                               
+                            }
+                            //console.log(response);          
+                        }, 
+                        error => {
+                            this.errorMessage = <any> error.error.message;
+                            console.log(error.error.message);
+                            
+                            this.errorMessageDiv = true;
+                            if (this.errorMessage != null) {              
+                                this.errorMessage = error.error.message;
+                                this.errorMessageDiv = true;
+                                console.log(error);              
+                            }
+                        }
+                    ); 
+                }
+                //console.log(response);          
+            }, 
+            error => {
+                this.errorMessage = <any> error.error.message;
+                console.log(error.error.message);
+                
+                this.errorMessageDiv = true;
+                if (this.errorMessage != null) {              
+                    this.errorMessage = error.error.message;
+                    this.errorMessageDiv = true;
+                    console.log(error);              
+                }
+            }
+        ); 
+    }
 }
--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { GLOBAL } from './global';
@@ -13,17 +13,25 @@ export class UserService {
         this.url = GLOBAL.url;
     }
 
-    signup(user_to_login: any, gethash = null){
+    signup(user_to_login: any, gethash: boolean = false) {
+
         /* return 'Hola mundo'; */
-        if (gethash != null) {
+        if (gethash != false) {
             user_to_login.gethash = gethash;
         }
+        /* console.log(user_to_login.email);
+        console.log(user_to_login.password); */
+
         let json = JSON.stringify(user_to_login);
         let params = json;
 
-        let headers = new HttpHeaders({'Content-Type':'application/json'});
+        /* const params = new HttpParams()
+            .set('email', user_to_login.email)
+            .set('password', user_to_login.password); */
 
-        return this._http.post( this.url+'login', params, {headers: headers})
-                          .pipe(map((res: any) => res.json ));
+        let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+        return this._http.post(this.url + 'login', params, { headers: headers })
+                         .pipe(map(res => res));
     }
 }
